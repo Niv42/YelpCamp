a647b2a31cacf609f9166d3e2b048a51a5ac2204
Niv Monovich <niv313@gmail.com>
2021-02-28 22:54:30 +0200

    add start script

--- app.js
+++ app.js
@@ -164,4 +164,6 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, () => console.log(`Running on port 3000`));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Running on port ${port}`));
